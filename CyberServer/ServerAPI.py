a3eb0c6a43c8bed60bbf003b57af5db603afd3c9
Zaituny <mohamedinv.580@gmail.com>
2024-12-27 21:56:07 +0200

    refactored code

--- CyberServer/ServerAPI.py
+++ CyberServer/ServerAPI.py
@@ -14,11 +14,11 @@ from Modules.Authentication import ServerAuth
 class ServerAPI:
     def __init__(self, host="127.0.0.1", port=12345):
         """
-		Initialize and start the server.
-		:param host: Host address to bind the server (default: 127.0.0.1)
-		:param port: Port to listen on (default: 12345)
-		"""
-        
+        Initialize and start the server.
+        :param host: Host address to bind the server (default: 127.0.0.1)
+        :param port: Port to listen on (default: 12345)
+        """
+
         self.host = host
         self.port = port
         self.auth = ServerAuth()
@@ -51,76 +51,77 @@ class ServerAPI:
                     print(f"Client {client_address} disconnected.")
                     break
                 print(f"Received from {client_address}: {data}")
-                self.handle_user_command(data, client_socket, client_address)
+                self.handle_user_command(data, client_socket)
         except Exception as e:
             print(f"Error with client {client_address}: {e}")
         finally:
             client_socket.close()
             print(f"Connection with {client_address} closed.")
 
-	def handle_user_command(self, data, client_socket):
-		command = json.loads(data)
-		if command.get("action") == "exit":
-			self.close_server()
-		elif command.get("action") == "register":
-			self.handle_user_registration(data, client_socket)
-		elif command.get("action") == "authenticate":
-			user_name = command.get("username")
-			# Authenticate a user
-			ServerAuth().authenticate_user(client_socket, user_name)
-		else:
-			print(f"Command '{command[0]}' not recognized.")
+    def handle_user_command(self, data, client_socket):
+        command = json.loads(data)
+        if command.get("action") == "exit":
+            self.close_server()
+        elif command.get("action") == "register":
+            self.handle_user_registration(data, client_socket)
+        elif command.get("action") == "authenticate":
+            user_name = command.get("username")
+            # Authenticate a user
+            ServerAuth().authenticate_user(client_socket, user_name)
+        else:
+            print(f"Command '{command[0]}' not recognized.")
 
-	def handle_user_registration(self, data, client_socket):
-		"""Handle user registration."""
-		try:
-			command = json.loads(data)
-			if command.get("action") == "register":
-				username = command["username"]
-				password = command["password"]
-				email = command["email"]
-				client_public_key = command["public_key"]
-				device_id = command["device_id"]
+    def handle_user_registration(self, data, client_socket):
+        """Handle user registration."""
+        try:
+            command = json.loads(data)
+            if command.get("action") == "register":
+                username = command["username"]
+                password = command["password"]
+                email = command["email"]
+                client_public_key = command["public_key"]
+                device_id = command["device_id"]
 
-				code = self.generate_secure_code()
-				EmailSender.send_email(email, code)
-				print(f"Registration code sent to {email}")
+                code = self.generate_secure_code()
+                EmailSender.send_email(email, code)
+                print(f"Registration code sent to {email}")
 
-				# add the code to the start of the public key and hash it
-				public_key_with_code = code + client_public_key
-				public_key_with_code_hashed = Hashing.hash_data(public_key_with_code.encode('utf-8'))
+                # add the code to the start of the public key and hash it
+                public_key_with_code = code + client_public_key
+                public_key_with_code_hashed = Hashing.hash_data(public_key_with_code.encode('utf-8'))
 
-				# Send the code concatenated with the public key hash to the client
-				response = json.dumps({"public_key_hash": public_key_with_code_hashed})
-				client_socket.send(response.encode('utf-8'))
+                # Send the code concatenated with the public key hash to the client
+                response = json.dumps({"public_key_hash": public_key_with_code_hashed})
+                client_socket.send(response.encode('utf-8'))
 
-				# receive the code from the user
-				encrypted_data = client_socket.recv(1024)
-				encrypted_data = encrypted_data.decode('utf-8')
-				server_private_key = KeyManagement.get_rsa_private_key('server@secure.org')
-				client_public_key_obj = RSA.import_key(client_public_key)
-				decrypted_data = KeyManagement.double_decrypt(encrypted_data, server_private_key, client_public_key_obj)
-				decrypted_data = decrypted_data.decode('utf-8')
-				print('decrypted code at the server:', decrypted_data)
+                # receive the code from the user
+                encrypted_data = client_socket.recv(1024)
+                encrypted_data = encrypted_data.decode('utf-8')
+                server_private_key = KeyManagement.get_rsa_private_key('server@secure.org')
+                client_public_key_obj = RSA.import_key(client_public_key)
+                decrypted_data = KeyManagement.double_decrypt(encrypted_data, server_private_key,
+                                                              client_public_key_obj)
+                decrypted_data = decrypted_data.decode('utf-8')
+                print('decrypted code at the server:', decrypted_data)
 
-				if decrypted_data == code:
-					# Register the user
-					self.auth.register_user(username, password, email, client_public_key, device_id)
-					print("User registered successfully.")
-				else:
-					print("Registration code mismatch. Registration failed.")
-			else:
-				print("Invalid command action.")
-		except json.JSONDecodeError:
-			print("Invalid JSON format received.")
+                if decrypted_data == code:
+                    # Register the user
+                    self.auth.register_user(username, password, email, client_public_key, device_id)
+                    print("User registered successfully.")
+                else:
+                    print("Registration code mismatch. Registration failed.")
+            else:
+                print("Invalid command action.")
+        except json.JSONDecodeError:
+            print("Invalid JSON format received.")
 
-	@staticmethod
-	def generate_secure_code(length=8):
-		# Define the characters to choose from for better entropy
-		characters = string.ascii_letters + string.digits
-		# Generate the code by cryptographically secure random selection
-		code = ''.join(choice(characters) for _ in range(length))
-		return code
+    @staticmethod
+    def generate_secure_code(length=8):
+        # Define the characters to choose from for better entropy
+        characters = string.ascii_letters + string.digits
+        # Generate the code by cryptographically secure random selection
+        code = ''.join(choice(characters) for _ in range(length))
+        return code
 
     def close_server(self):
         """Close the server socket."""
@@ -143,4 +144,3 @@ class ServerAPI:
 if __name__ == "__main__":
     server = ServerAPI()
     server.start()
-
