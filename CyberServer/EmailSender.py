cfe3248c0bebb56b7444630f390b8d2e09cdd670
KareemWael1 <kareem25102002@gmail.com>
2024-12-27 23:55:39 +0200

    Implement email service

--- CyberServer/EmailSender.py
+++ CyberServer/EmailSender.py
@@ -1,6 +1,27 @@
-def send_email(email, message):
-    """Send an email to the specified email address."""
-    print(f"Sending email to {email}")
-    print(f"Message: {message}")
-    print("Email sent.")
-    return True
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_email(recipient_email, message_text):
+    sender_email = os.getenv("EMAIL_SERVICE_USER")
+    sender_password = os.getenv("EMAIL_SERVICE_PASSWORD")
+    smtp_server = os.getenv("EMAIL_SERVER")
+    smtp_port = os.getenv("EMAIL_PORT")
+
+    try:
+        message_text = "Your registration code is " + message_text
+        msg = MIMEText(message_text)
+        msg['Subject'] = "Cybertools Registration Code"
+        msg['From'] = sender_email
+        msg['To'] = recipient_email
+
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(sender_email, sender_password)
+            server.send_message(msg)
+            print("Email sent successfully!")
+            return True
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+        return False
